661414dbe4dd494fd87ba0807330a3f780049e30
nazibot1 <42460638+nazibot1@users.noreply.github.com>
2018-09-16 03:16:07 +0300

    Add files via upload

---
+++ bc.js
@@ -0,0 +1,4 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login("NDY1NzAxMTAwMzUyMTc2MTI4.DnsYLw.yWRzHfQzRCr1JPWz88wIrh9tyCw");
---
+++ s.bat
@@ -0,0 +1,2 @@
+node bc.js
+paused
